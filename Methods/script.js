8f4bada650ee81e98b3f8235d14fb57c4cdc2b38
Lalit Garghate <lalit.garghate@gmail.com>
2020-11-11 09:52:00 +0530

    this in arrow fun in object method

--- Methods/script.js
+++ Methods/script.js
@@ -6,12 +6,19 @@ let profile1 = {
   name: "Lalit",
   desg: "SE",
   show: function(company, city) {
-    console.log(this);
+    console.log(this); //refers to owner object
+    console.log(`${this.name} works at ${company} as ${this.desg} in ${city}`);
+  },
+
+  //Note : In Object method if we use arrow function then it will refers to window object
+  disp: (company, city) => {
+    console.log(this); //refers to window object
     console.log(`${this.name} works at ${company} as ${this.desg} in ${city}`);
   }
 };
 
 profile1.show("XYZ", "Pune");
+// profile1.disp("XYZ", "Pune");
 
 let profile2 = {
   name: "Nikhil",
