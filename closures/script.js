ff489afb927ab46ee5b8b1e35ddfacf210f9cfe1
Lalit Garghate <lalit.garghate@gmail.com>
2021-03-22 12:08:47 +0530

    closure ex -2

--- closures/script.js
+++ closures/script.js
@@ -1,11 +1,19 @@
 //closures
+// ex-1
+// function a() {
+//   let x = 10;
+//   function b() {
+//     console.log(x);
+//   }
+//   b();
+// }
+// a();
 
-function a() {
-  let x = 10;
-  function b() {
-    console.log(x);
-  }
-  b();
-}
-
-a();
+// ex-2
+// const a = () => {
+//   const val = 90;
+//   return () => console.log(val);
+// };
+// const res = a();
+// console.log(res);
+// res(); //op = 90
