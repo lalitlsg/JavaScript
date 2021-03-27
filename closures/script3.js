669eb9ab0fd714e0feeeb23de8651202aaa71a28
Lalit Garghate <lalit.garghate@gmail.com>
2021-03-27 12:29:16 +0530

    css priority

--- closures/script3.js
+++ closures/script3.js
@@ -1,18 +1,18 @@
 // setTimeout does not always run after specific amount of time.
 
-console.log("Start");
+// console.log("Start");
 
-setTimeout(() => {
-  console.log("Callback");
-}, 3000);
+// setTimeout(() => {
+//   console.log("Callback");
+// }, 3000);
 
-console.log("End");
+// console.log("End");
 
 //block main thread
 
-const startDate = new Date().getTime();
-let endDate = startDate;
-while (endDate < startDate + 10000) {
-  endDate = new Date().getTime();
-}
-console.log("Main Thread Execution Complete");
+// const startDate = new Date().getTime();
+// let endDate = startDate;
+// while (endDate < startDate + 10000) {
+//   endDate = new Date().getTime();
+// }
+// console.log("Main Thread Execution Complete");
