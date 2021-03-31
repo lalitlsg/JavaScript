7f0abcc8a4492ef3fdd53319bc8c176337144b0c
Lalit Garghate <lalit.garghate@gmail.com>
2021-03-31 20:59:37 +0530

    seperating function from obj and make it common

--- core-js/script.js
+++ core-js/script.js
@@ -1,18 +1,41 @@
+// let obj1 = {
+//   firstname: "lalit",
+//   lastname: "garghate",
+//   print: function () {
+//     console.log(`${this.firstname} ${this.lastname}`);
+//   },
+// };
+
+// obj1.print();
+
+// let obj2 = {
+//   firstname: "sam",
+//   lastname: "walton",
+// };
+
+// //function borrowing
+// //using call method
+// obj1.print.call(obj2);
+
+//seperating function from obj and make it common
+
 let obj1 = {
   firstname: "lalit",
   lastname: "garghate",
-  print: function () {
-    console.log(`${this.firstname} ${this.lastname}`);
-  },
 };
-
-obj1.print();
-
 let obj2 = {
   firstname: "sam",
   lastname: "walton",
 };
 
-//function borrowing
-//using call method
-obj1.print.call(obj2);
+// will give undefine cause arrow fun does not have its own this.
+// const print = () => {
+//   console.log(`${this.firstname} ${this.lastname}`);
+// };
+
+const print = function () {
+  console.log(`${this.firstname} ${this.lastname}`);
+};
+
+print.call(obj1);
+print.call(obj2);
